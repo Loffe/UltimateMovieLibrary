b1f19b4bce07155bf6b4a3ce31f2872e3ec43b83
Erik Eloff <eriel743@student.liu.se>
2011-04-11 14:25:26 +0200

    First window

---
+++ src/se/eloff/ultimatemovielibrary/AppFrame.java
@@ -0,0 +1,14 @@
+package se.eloff.ultimatemovielibrary;
+
+import java.awt.HeadlessException;
+
+import javax.swing.JFrame;
+
+public class AppFrame extends JFrame {
+
+    private static final long serialVersionUID = 5297734322373835993L;
+
+    public AppFrame() throws HeadlessException {
+        this.setSize(800, 600);
+    }
+}
---
+++ src/se/eloff/ultimatemovielibrary/Main.java
@@ -0,0 +1,13 @@
+package se.eloff.ultimatemovielibrary;
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		AppFrame app = new AppFrame();
+		app.setVisible(true);
+	}
+
+}
