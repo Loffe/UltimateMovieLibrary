5a463889c9bb9983a4a93088f651b9dd24eeb748
Erik Eloff <erik@eloff.se>
2011-04-20 10:09:57 +0200

    Add Menubar class

---
+++ src/se/eloff/ultimatemovielibrary/Menubar.java
@@ -0,0 +1,9 @@
+package se.eloff.ultimatemovielibrary;
+
+import javax.swing.JPanel;
+
+public class Menubar extends JPanel {
+
+    private static final long serialVersionUID = -8430416772154328684L;
+
+}
