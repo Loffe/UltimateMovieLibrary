6aea21928cbf7efaf447f1d19b55c59af74da9c0
Erik Eloff <eriel743@student.liu.se>
2011-05-02 12:17:57 +0200

    Add stub header

--- src/se/eloff/ultimatemovielibrary/ResultPanel.java
+++ src/se/eloff/ultimatemovielibrary/ResultPanel.java
@@ -3,11 +3,13 @@ package se.eloff.ultimatemovielibrary;
 import java.util.List;
 
 import javax.swing.BoxLayout;
+import javax.swing.JComponent;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 
-public abstract class ResultPanel extends JScrollPane implements MovieSearchClient {
+public abstract class ResultPanel extends JScrollPane implements
+        MovieSearchClient {
 
     private static final long serialVersionUID = -7751458552907067506L;
     private JPanel resultPanel;
@@ -17,7 +19,20 @@ public abstract class ResultPanel extends JScrollPane implements MovieSearchClie
         resultPanel = new JPanel();
         resultPanel.setLayout(new BoxLayout(resultPanel, BoxLayout.PAGE_AXIS));
         setViewportView(resultPanel);
+
+        setColumnHeaderView(createHeader());
         getVerticalScrollBar().setUnitIncrement(20);
+    }
+
+    private JComponent createHeader() {
+        JPanel header = new JPanel();
+        header.setLayout(new BoxLayout(header, BoxLayout.LINE_AXIS));
+        header.add(new JLabel("Titel"));
+        header.add(new JLabel("|"));
+        header.add(new JLabel("Sedd"));
+        header.add(new JLabel("|"));
+        header.add(new JLabel("Betyg"));
+        return header;
     }
 
     @Override
@@ -35,5 +50,6 @@ public abstract class ResultPanel extends JScrollPane implements MovieSearchClie
             revalidate();
         }
     }
+
     public abstract void search();
 }
