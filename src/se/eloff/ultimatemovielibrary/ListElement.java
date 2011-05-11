1c8b17ae11e157a21d729eeb14d394146e04eea1
David Erenger <david.erenger@gmail.com>
2011-05-11 16:46:32 +0200

    Moves the playlist menu 5 pixels!

--- src/se/eloff/ultimatemovielibrary/ListElement.java
+++ src/se/eloff/ultimatemovielibrary/ListElement.java
@@ -189,7 +189,7 @@ public class ListElement extends javax.swing.JPanel {
 
                 // playlistMenu.setInvoker(this);
 
-                playlistMenu.show(playlistButton, 0, 0 + 55);
+                playlistMenu.show(playlistButton, 0, 0 + 50);
 
             }
         });
