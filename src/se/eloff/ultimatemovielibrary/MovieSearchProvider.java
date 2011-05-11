bdad733c39d636c6a376660bf205528853628363
Erik Eloff <eriel743@student.liu.se>
2011-05-11 13:46:07 +0200

    Format query better

--- src/se/eloff/ultimatemovielibrary/MovieSearchProvider.java
+++ src/se/eloff/ultimatemovielibrary/MovieSearchProvider.java
@@ -114,9 +114,6 @@ public class MovieSearchProvider {
                         order_clause += ascending ? " ASC" : " DESC";
                     }
 
-                    // Sorry Elof, guess you have to fix it again, but this
-                    // returns correct result for all movies as well even if they
-                    // are in no list
                     String sql;
                     String list_id;
                     if (list != null) {
@@ -126,21 +123,18 @@ public class MovieSearchProvider {
                                 + " from movies_lists ml"
                                 + " left join movies m on ml.movie_id = m.id"
                                 + " where discnumber = 1"
-                                + " and list_id = "
-                                + list_id
-                                + " and name like '%"
-                                + name
-                                + "%'"
-                                + " order by " + order_clause;
+                                + " and list_id = %s"
+                                + " and name like '%%%s%%'    "
+                                + " order by %s";
+                        sql = String.format(sql, list_id, name, order_clause);
                     } else {
 
                         sql = "select id, name, year, filepath, discnumber, rating, seen"
                                 + " from movies"
                                 + " where discnumber = 1"
-                                + " and name like '%"
-                                + name
-                                + "%'"
-                                + " order by " + order_clause;
+                                + " and name like '%%%s%%'    "
+                                + " order by %s";
+                        sql = String.format(sql, name, order_clause);
                     }
 
                     RawRowMapper<LocalMovie> rowMapper = new RawRowMapper<LocalMovie>() {
@@ -149,8 +143,8 @@ public class MovieSearchProvider {
                                 String[] resultColumns) throws SQLException {
                             LocalMovie m = new LocalMovie(resultColumns[1],
                                     Integer.parseInt(resultColumns[2]),
-                                    resultColumns[3],
-                                    Integer.parseInt(resultColumns[4]),
+                                    resultColumns[3], Integer
+                                            .parseInt(resultColumns[4]),
                                     Integer.parseInt(resultColumns[5]));
                             boolean seen = resultColumns[6].equals("1");
                             m.setId(Integer.parseInt(resultColumns[0]));
@@ -188,8 +182,8 @@ public class MovieSearchProvider {
                     while (randomMovies.size() < numberOfMovies && rating > -1) {
                         QueryBuilder<LocalMovie, Integer> queryBuilder = dbMovie
                                 .queryBuilder();
-                        queryBuilder.where().eq("discnumber", 1).and()
-                                .eq("seen", false).and().eq("rating", rating);
+                        queryBuilder.where().eq("discnumber", 1).and().eq(
+                                "seen", false).and().eq("rating", rating);
 
                         List<LocalMovie> movies = dbMovie.query(queryBuilder
                                 .prepare());
