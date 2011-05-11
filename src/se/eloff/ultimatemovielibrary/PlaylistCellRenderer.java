ef5a2b13d63305d7ef0548c8be86129ececf3b44
Christoffer Nylén <christoffer.nylen@hotmail.com>
2011-05-11 15:05:10 +0200

    PlaylistCell

---
+++ src/se/eloff/ultimatemovielibrary/PlaylistCellRenderer.java
@@ -0,0 +1,34 @@
+package se.eloff.ultimatemovielibrary;
+
+import java.awt.Color;
+import java.awt.Component;
+
+
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.ListCellRenderer;
+
+public class PlaylistCellRenderer extends JLabel implements ListCellRenderer {
+    public PlaylistCellRenderer() {
+        setOpaque(true);
+    }
+    
+    
+
+    public Component getListCellRendererComponent(JList list, Object value,
+            int index, boolean isSelected, boolean cellHasFocus) {
+        if(value.toString().equals(Localization.profileFavoriteList))
+            setIcon(Localization.listsFavoriteIcon);
+        else if(value.toString().equals(Localization.profileWishList))
+            setIcon(Localization.listsWishIcon);
+        else if(value.toString().equals(Localization.profileSeenList))
+            setIcon(Localization.listsSeenIcon);
+        else
+            setIcon(new ImageIcon());
+        setText(value.toString());
+        setBackground(isSelected ? Color.red : Color.white);
+        setForeground(isSelected ? Color.white : Color.black);
+        return this;
+    }
+}
