fa9dbbdad1dd080d1cd03e8eb69b1230db343585
Erik Eloff <erik@eloff.se>
2011-05-14 12:26:04 +0200

    WindowAdapter instead of WindowListener

--- src/se/eloff/ultimatemovielibrary/AppFrame.java
+++ src/se/eloff/ultimatemovielibrary/AppFrame.java
@@ -12,8 +12,8 @@ import java.awt.HeadlessException;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
-import java.awt.event.WindowListener;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
@@ -58,35 +58,10 @@ public class AppFrame extends JFrame implements ActionListener {
         this.setExtendedState(this.getExtendedState() | JFrame.MAXIMIZED_BOTH);
 
         watchFolderManagerPanel = new WatchFolderManagerDialog(this);
-        watchFolderManagerPanel.addWindowListener(new WindowListener() {
-
-            @Override
-            public void windowOpened(WindowEvent e) {
-            }
-
-            @Override
-            public void windowIconified(WindowEvent e) {
-            }
-
-            @Override
-            public void windowDeiconified(WindowEvent e) {
-            }
-
-            @Override
-            public void windowDeactivated(WindowEvent e) {
-            }
-
+        watchFolderManagerPanel.addWindowListener(new WindowAdapter() {
             @Override
             public void windowClosing(WindowEvent e) {
                 updateCurrentPanel();
-            }
-
-            @Override
-            public void windowClosed(WindowEvent e) {
-            }
-
-            @Override
-            public void windowActivated(WindowEvent e) {
             }
         });
 
